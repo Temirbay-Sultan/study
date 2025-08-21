315a0bc63941f807b0ae6556015aab446c8d112f
Sultan-assasin <sulltanai@gmail.com>
2025-08-21 16:26:16 +0500

    function calculate ready

--- javascript-site/script.js
+++ javascript-site/script.js
@@ -1,7 +1,10 @@
+
 const display = document.getElementById("display");
 const numbers = document.querySelectorAll(".btn");
 const cleanAll = document.getElementById("clean");
 const result = document.getElementById("result");
+const minus = document.getElementById("minus");
+const dot = document.getElementById("dot");
 
 function updateClock() {
   const now = new Date();
@@ -28,13 +31,17 @@ numbers.forEach((button) => {
     } else {
       display.textContent += value;
     }
-    if (display.textContent.length > 9) {
+    readNumber();
+  });
+});
+
+function readNumber(){
+  if (display.textContent.length > 9) {
       display.style.fontSize = "60px"; 
     } else {
       display.style.fontSize = "93px";
     }
-  });
-});
+}
 
 cleanAll.addEventListener("click", () => {
   display.textContent = 0;
@@ -43,6 +50,39 @@ cleanAll.addEventListener("click", () => {
 
 result.addEventListener("click", () => {
   let res = display.textContent;
+  res = res.replace(/=/g, "");
+
+  res = res.replace(/×/g, "*");
+
+  res = res.replace(/÷/g, "/");
+  
+
+  try {
+    display.textContent = math.evaluate(res);
+  } catch (err) {
+    display.textContent = "Error";
+    console.error(err);
+  }
 });
 
+minus.addEventListener("click", () => {
+  let expr = display.textContent;
 
+  let match = expr.match(/(\d+)(?!.*\d)/);
+
+  if (match) {
+    let number = match[0];
+
+    if (expr.includes(`(-${number})`)) {
+      display.textContent = expr.replace(`(-${number})`, number);
+    } else {
+      let replaced = `(-${number})`;
+      display.textContent =
+        expr.slice(0, match.index) + replaced;
+    }
+  }
+});
+
+dot.addEventListener("click",() =>{
+
+})
